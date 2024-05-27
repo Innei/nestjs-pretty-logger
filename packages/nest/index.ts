0830d4493e642ac775db6e975f68ea32ab1f8f0c
Innei <i@innei.in>
2024-05-27 13:01:07 +0800

    chore: export
    Signed-off-by: Innei <i@innei.in>

--- packages/nest/index.ts
+++ packages/nest/index.ts
@@ -1,6 +1,9 @@
-import { createLoggerConsola } from '@innei/pretty-logger-core'
-
 export { Logger } from './logger.service.js'
 export { LoggerModule } from './logger.module.js'
-export { type LoggerConsolaOptions } from '@innei/pretty-logger-core'
-export const createLogger = createLoggerConsola
+export {
+  type LoggerConsolaOptions,
+  createLoggerConsola as createLogger,
+  type WrappedConsola,
+} from '@innei/pretty-logger-core'
+
+export * as Core from '@innei/pretty-logger-core'
